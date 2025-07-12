09da1d1d2c098513c83b1bf9ae502535374f2d94
Maksim Romanov <maksim.romanov@jetrockets.com>
2025-07-12 14:05:55 +0300

    chore: project structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const importPlugin = require("eslint-plugin-import");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
+  importPlugin.flatConfigs["react-native"],
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*", ".expo/*", "node_modules/*"],
+  },
+  {
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["parent", "sibling"]],
+          pathGroups: [
+            { pattern: "react", group: "builtin", position: "before" },
+          ],
+          pathGroupsExcludedImportTypes: ["react"],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+    },
   },
 ]);
