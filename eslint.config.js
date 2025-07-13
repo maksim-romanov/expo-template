11390837836553693b815ab8386868631e5bc7a6
Maksim Romanov <maksim.romanov@jetrockets.com>
2025-07-13 14:31:55 +0300

    chore: add json eslint support

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
 const cspellConfigs = require("@cspell/eslint-plugin/configs");
+const json = require("@eslint/json").default;
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const importPlugin = require("eslint-plugin-import");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
-  expoConfig,
   eslintPluginPrettierRecommended,
-  importPlugin.flatConfigs["react-native"],
   cspellConfigs.recommended,
   {
     ignores: ["dist/*", ".expo/*", "node_modules/*"],
   },
   {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    extends: [importPlugin.flatConfigs["react-native"], expoConfig],
     rules: {
       "import/order": [
         "error",
@@ -27,5 +28,12 @@ module.exports = defineConfig([
       ],
       "prettier/prettier": ["error", { printWidth: 120 }],
     },
+  },
+  {
+    files: ["**/*.json"],
+    ignores: ["package-lock.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
   },
 ]);
